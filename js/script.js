f40e5776676425badd77c3de6c6b39eba35cd355
7chillgo-tech <akebi.chinatsu@gmail.com>
2026-08-06 14:23:50 +0900

    ヘッダースクロールの修正

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
 // js/script.js
 
 const header = document.querySelector('header');
-let scrollTimer;
 
 window.addEventListener('scroll', () => {
-    header.classList.add('is-hidden');   // スクロール中は透明に
-
-    clearTimeout(scrollTimer);           // タイマーをリセット
-    scrollTimer = setTimeout(() => {
-        header.classList.remove('is-hidden'); // 止まって300ms後に表示
-    }, 300);
-
     if (window.scrollY > 0) {
         header.classList.add('is-scrolled');
     } else {
